18b75a74e0abaf83a246c46d4d8cac1000572103
nickdaniels <n1daniels@student.bridgew.edu>
2022-02-07 09:42:01 -0500

    Peer Assist

---
+++ main 2.py
@@ -0,0 +1,7 @@
+def main():
+    import math
+    user_imput1 = int(input("what is the height"))
+    user_imput2 = int(input("what is the width"))
+    print(f"your area would be {user_imput2*user_imput1}")
+    print(f"your perimeter would be {(user_imput1*2)+(user_imput1*2)}")
+main()
--- main.py
+++ main.py
@@ -5,7 +5,13 @@ def main():
     half_age = user_response/2
     print(f"{half_age} is your half age")
 
-    # maybe im doing this right
+    print("the the the the the the facebook the the the")
+    demo = "the the the the the the facebook the the the"
+    how_many = demo.count("the")
+    print(f"the has showed up {how_many} time")
+    capital_version = demo.upper()
+    print(capital_version)
+    import math
 
 main()
 
